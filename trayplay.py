fe51900f76bd0556373e134e4a675b58164d06bb
squigglyosrs <brianbis+yzerd@gmail.com>
2026-03-01 19:33:57 -0700

    Auto-commit: Claude edited files

--- trayplay.py
+++ trayplay.py
@@ -2150,8 +2150,9 @@ class AirPlayTray:
                 win, from_=100, to=0, orient=tk.VERTICAL,
                 length=slider_len, width=14, sliderlength=18,
                 resolution=1, showvalue=False,
+                label="Vol",
                 bg="#2b2b2b", fg="white", troughcolor="#555",
-                highlightthickness=0, bd=0,
+                highlightthickness=2, highlightcolor="#6688cc", bd=0,
                 activebackground="#999",
             )
             slider.set(int(current))
